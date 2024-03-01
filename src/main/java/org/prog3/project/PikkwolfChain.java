c39e0dd34c9fd502f384f0c7fab5a1b529bc47e6
Joana311 <deptop725+3@gmail.com>
2024-03-02 00:06:49 +0100

    added

--- src/main/java/org/prog3/project/PikkwolfChain.java
+++ src/main/java/org/prog3/project/PikkwolfChain.java
@@ -55,35 +55,31 @@ public class PikkwolfChain {
         // testing
         Block block1 = new Block(genesis.hash);
         System.out.println("\nWalletA's balance is: " + walletA.getBalance());
-        System.out.println("\nWalletA is Attempting to send funds (40) to WalletB...");
+        System.out.println("WalletA is Attempting to send funds (40) to WalletB...");
         block1.addTransaction(walletA.sendFunds(walletB.publicKey, 40f));
         addBlock(block1);
+
         System.out.println("\nWalletA's balance is: " + walletA.getBalance());
         System.out.println("WalletB's balance is: " + walletB.getBalance());
 
-        Block block3 = new Block(block1.hash);
+        Block block2 = new Block(block1.hash);
+        System.out.println("\nWalletA Attempting to send more funds (1000) than it has...");
+        block2.addTransaction(walletA.sendFunds(walletB.publicKey, 1000f));
+        addBlock(block2);
+
+        System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+        System.out.println("WalletB's balance is: " + walletB.getBalance());
+
+        Block block3 = new Block(block2.hash);
         System.out.println("\nWalletB is Attempting to send funds (20) to WalletA...");
         block3.addTransaction(walletB.sendFunds(walletA.publicKey, 20));
+
         System.out.println("\nWalletA's balance is: " + walletA.getBalance());
         System.out.println("WalletB's balance is: " + walletB.getBalance());
 
         isChainValid();
 
         // TODO: add a socket
-
-        System.err.println("helo");
-        ArrayList<Client> clients = new ArrayList<>();
-        try {
-            ServerSocket server = new ServerSocket(5555);
-            while (true) {
-                Socket connection = server.accept();
-                Client newClient = new Client(connection, clients, blockchain);
-                clients.add(newClient);
-                executorService.submit(newClient);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static void isChainValid() {
