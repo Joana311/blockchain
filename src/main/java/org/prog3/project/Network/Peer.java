b28d9212d8d6582a3ff472f82016309312b335a6
Joana311 <deptop725+3@gmail.com>
2024-05-17 17:57:39 +0200

    wanted to generate keys here, it is wrong place I think

--- src/main/java/org/prog3/project/Network/Peer.java
+++ src/main/java/org/prog3/project/Network/Peer.java
@@ -2,8 +2,14 @@ package org.prog3.project.Network;
 
 import java.io.*;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 import lombok.Getter;
 import lombok.Setter;
+import org.prog3.project.Message.Message;
+import org.prog3.project.Protocols.Protocol;
 
 
 @Getter
@@ -13,6 +19,7 @@ public class Peer implements Runnable {
     private BufferedReader in;
     private BufferedWriter out;
     private NetworkManager networkManager;
+    private Protocol protocol;
 
     public Peer(Socket connection, NetworkManager networkManager) {
         this.networkManager = networkManager;
@@ -32,6 +39,9 @@ public class Peer implements Runnable {
         try {
             while ((msg = in.readLine()) != null) {
                 System.out.println("Msg: " + msg);
+                // TODO: fix the protocol, probably need to pass it as value from/to somewhere
+                Message message = new Message(msg, protocol);
+                // TODO: if the message is valid (verified) broadcast it, otherwise error
                 networkManager.broadcast(msg);
             }
         } catch (IOException e) {
@@ -57,6 +67,18 @@ public class Peer implements Runnable {
         } catch (IOException e) {
 //            e.printStackTrace();
             throw new RuntimeException(e);
+        }
+    }
+
+    private void generateKeys() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(2048);
+            KeyPair pair = keyGen.generateKeyPair();
+//            this.privateKey = pair.getPrivate();
+//            this.publicKey = pair.getPublic();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("RSA algorithm not found", e);
         }
     }
 }
