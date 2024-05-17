b9259fca352d9c4b3414d5fe9492fa40e803faad
Joana311 <deptop725+3@gmail.com>
2024-05-17 17:57:00 +0200

    moved the function here

--- src/main/java/org/prog3/project/Protocols/PeerDiscovery.java
+++ src/main/java/org/prog3/project/Protocols/PeerDiscovery.java
@@ -1,4 +1,7 @@
 package org.prog3.project.Protocols;
 
 public class PeerDiscovery {
+    public void broadcast(String msg) {
+//        peers.values().parallelStream().forEach(peer -> peer.send(msg));
+    }
 }
