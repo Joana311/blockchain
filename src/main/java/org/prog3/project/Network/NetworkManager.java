5f72d66cee75f4af0af98ec97a5ba4e9c7d941d2
Joana311 <deptop725+3@gmail.com>
2024-05-17 17:55:41 +0200

    canged constructor, need to finish the thread I call it

--- src/main/java/org/prog3/project/Network/NetworkManager.java
+++ src/main/java/org/prog3/project/Network/NetworkManager.java
@@ -1,68 +1,60 @@
 package org.prog3.project.Network;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.prog3.project.Message.Message;
+import org.prog3.project.Message.MessageHeader;
+import org.prog3.project.Utilities.Constants;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.*;
 
+@Getter
+@Setter
 public class NetworkManager {
-    HashMap<String, Peer> peers;
-    Queue<Message> incoming;
-    Queue<Message> outgoing;
-    ExecutorService networkPool;
+    private String ip;
+    private boolean isTrusted = false;
+    private HashMap<String, Peer> peers;
+
+//    private Crypto crypto;
+//    private BlockingQueue<Message> messageQueue = new ArrayBlockingQueue<>(1024);
+//    private HashMap<String, PeerInfo> knownNodes;
+//    private PeerDiscovery peerDiscovery;
 
     // TODO: config this constructor
-    public NetworkManager() {
+    // TODO: network managed needs to hear for incoming messages
+    public NetworkManager(Constants constants) {
         peers = new HashMap<>();
-        incoming = new LinkedList<>();
-        outgoing = new LinkedList<>();
-        networkPool = Executors.newCachedThreadPool();
-
-        new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(12137)) {
-                while (true) {
-                    Peer peer = new Peer(serverSocket.accept(), this);
-                    networkPool.submit(peer);
-                    peers.put("" + peer.hashCode(), peer);
-                }
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-
-        new Thread(() -> {
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                String message = scanner.next();
-                peers.values().parallelStream().forEach(peer -> peer.send(message));
-            }
-        }).start();
-
-    }
-
-    public void connect(String address, int port) {
         try {
-            Peer p = new Peer(new Socket(address, port), this);
-            networkPool.submit(p);
-            peers.put("" + p.hashCode(), p);
-        } catch (IOException e) {
+            this.ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
+        if (ip.equals(constants.HOST))
+            this.isTrusted = true;
+        // TODO: discover peers here, and with the thread bellow process if message is incoming
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO: handle messages here
+            }
+        }).start();
     }
 
     public void broadcast(String msg) {
         peers.values().parallelStream().forEach(peer -> peer.send(msg));
     }
 
-    // TODO: function to create message to be sent
+    public Message createMessage(MessageHeader header, String body) {
+        // TODO: verify the message - set signature
 
-    // TODO: function for verifying message
+        return new Message(header, body);
+    }
+
+    public boolean verifyMessage(Message message) {
+        // TODO: make function in crypto to verify the message, but first need to set the signature
+        return true;
+    }
 }
