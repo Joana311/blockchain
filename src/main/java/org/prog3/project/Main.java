0f3bb3c8d93b172dfa1c80465f07a956286db6d0
Joana311 <deptop725+3@gmail.com>
2024-05-17 17:53:16 +0200

    ability for trusted node to boot

--- src/main/java/org/prog3/project/Main.java
+++ src/main/java/org/prog3/project/Main.java
@@ -5,20 +5,32 @@ import org.prog3.project.Network.Peer;
 import org.prog3.project.Utilities.Constants;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: Trusted node needs to be present, at least for booting the network
-        NetworkManager networkManager = new NetworkManager();
-        Socket socket;
-        try {
-            socket = new Socket(Constants.HOST, Constants.PORT);
-            Peer peer = new Peer(socket, networkManager);
-            peer.run();
+        Constants constants = new Constants();
+        NetworkManager networkManager = new NetworkManager(constants);
+        // connecting normal node, not "trusted"
+        if (!networkManager.isTrusted()) {
+            Socket socket;
+            try {
+                socket = new Socket(constants.HOST, constants.PORT);
+            } catch (IOException e) {
+                System.out.println("Client failed to connect\n");
+                throw new RuntimeException(e);
+            }
+            new Thread(new Peer(socket, networkManager)).start();
+            // TODO: what else? Peer discovery? Trusted? Send messages?
+        }
+        // booting from trusted node (only once - the first time)
+        try (ServerSocket serverSocket = new ServerSocket(constants.PORT)) {
+            System.out.println("Server listening on port: " + serverSocket.getLocalPort() + "\n");
+            while (true) new Thread(new Peer(serverSocket.accept(), networkManager)).start();
         } catch (IOException e) {
+            System.out.println("Server failed to start\n");
             throw new RuntimeException(e);
         }
-
     }
 }
