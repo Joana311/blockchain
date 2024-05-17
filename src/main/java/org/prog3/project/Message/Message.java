c790f14b1ee5c7d2dce7b65d93f8949696f55fc4
Joana311 <deptop725+3@gmail.com>
2024-05-17 19:31:14 +0200

    removed unnecessary constructor

--- src/main/java/org/prog3/project/Message/Message.java
+++ src/main/java/org/prog3/project/Message/Message.java
@@ -10,12 +10,6 @@ public class Message {
     MessageHeader header;
     String body;
 
-    // TODO: somehow need to add the private/public key or whatever
-    public Message(String body, Protocol protocol) {
-        this.body = body;
-        this.header = new MessageHeader("", "", "", protocol);
-    }
-
     public Message(MessageHeader header, String body) {
         this.header = header;
         this.body = body;
