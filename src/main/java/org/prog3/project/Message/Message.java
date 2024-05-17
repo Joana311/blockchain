4eee50b5d943471d2df70dedbbf3960514337fc5
Joana311 <deptop725+3@gmail.com>
2024-05-17 17:54:41 +0200

    changed content

--- src/main/java/org/prog3/project/Message/Message.java
+++ src/main/java/org/prog3/project/Message/Message.java
@@ -2,12 +2,20 @@ package org.prog3.project.Message;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.prog3.project.Protocols.Protocol;
 
 @Getter
 @Setter
 public class Message {
     MessageHeader header;
     String body;
+
+    // TODO: somehow need to add the private/public key or whatever
+    public Message(String body, Protocol protocol) {
+        this.body = body;
+        this.header = new MessageHeader("", "", "", protocol);
+    }
+
     public Message(MessageHeader header, String body) {
         this.header = header;
         this.body = body;
