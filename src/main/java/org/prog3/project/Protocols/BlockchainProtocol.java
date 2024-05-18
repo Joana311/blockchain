32e4458bd082744c5a9a355425dc97172ed099e7
Joana311 <deptop725+3@gmail.com>
2024-05-18 13:09:35 +0200

    added getter and setter, no changes

--- src/main/java/org/prog3/project/Protocols/BlockchainProtocol.java
+++ src/main/java/org/prog3/project/Protocols/BlockchainProtocol.java
@@ -1,10 +1,14 @@
 package org.prog3.project.Protocols;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.prog3.project.Message.Message;
 import org.prog3.project.Network.NetworkManager;
 
+@Getter
+@Setter
 public class BlockchainProtocol implements Protocol {
-    NetworkManager networkManager;
+    private NetworkManager networkManager;
 
     public BlockchainProtocol(NetworkManager networkManager) {
         this.networkManager = networkManager;
