f2fdf2185a5c1cfd72505329d79ba22c48aa0dd7
Joana311 <deptop725+3@gmail.com>
2024-05-18 23:49:15 +0200

    added peer property so it can be disconnected if protocol violation/message not valid

--- src/main/java/org/prog3/project/Message/MessageHeader.java
+++ src/main/java/org/prog3/project/Message/MessageHeader.java
@@ -2,6 +2,7 @@ package org.prog3.project.Message;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.prog3.project.Network.Peer;
 import org.prog3.project.Protocols.BlockchainProtocol;
 
 import java.security.PublicKey;
@@ -15,10 +16,12 @@ public class MessageHeader {
     private BlockchainProtocol protocol;
     private PublicKey publicKey;
     private String signature;
+    private Peer holder;
 
-    public MessageHeader(BlockchainProtocol protocol) {
+    public MessageHeader(BlockchainProtocol protocol, Peer peer) {
         this.timestamp = currentTimeMillis();
         this.protocol = protocol;
+        this.holder = peer;
     }
 
 }
