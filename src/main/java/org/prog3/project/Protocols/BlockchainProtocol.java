46e2774da04540af5f0db277e9c365b3c249dc1b
Joana311 <deptop725+3@gmail.com>
2024-05-18 23:16:04 +0200

    added protocol type, ping/pong for discovery later, request/response for message send

--- src/main/java/org/prog3/project/Protocols/BlockchainProtocol.java
+++ src/main/java/org/prog3/project/Protocols/BlockchainProtocol.java
@@ -10,12 +10,19 @@ import org.prog3.project.Network.NetworkManager;
 public class BlockchainProtocol implements Protocol {
     private NetworkManager networkManager;
 
-    public BlockchainProtocol(NetworkManager networkManager) {
+    public enum Type {
+        PING, PONG, REQUEST, RESPONSE
+    }
+
+    private Type type;
+
+    public BlockchainProtocol(Type type, NetworkManager networkManager) {
         this.networkManager = networkManager;
+        this.type = type;
     }
 
     @Override
     public void digest(Message message) {
-        // TODO: do something useful here
+
     }
 }
--- src/main/java/org/prog3/project/Protocols/Protocol.java
+++ src/main/java/org/prog3/project/Protocols/Protocol.java
@@ -3,10 +3,6 @@ package org.prog3.project.Protocols;
 import org.prog3.project.Message.Message;
 
 public interface Protocol {
-    enum Type {
-        PING,
-        PONG,
-        NORMAL
-    }
+
     public void digest (Message message);
 }
