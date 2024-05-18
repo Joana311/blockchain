30eb3d018c3d94acbb772f4647b9d96be886d95e
Joana311 <deptop725+3@gmail.com>
2024-05-18 13:09:11 +0200

    should I use enums?

--- src/main/java/org/prog3/project/Protocols/Protocol.java
+++ src/main/java/org/prog3/project/Protocols/Protocol.java
@@ -3,5 +3,10 @@ package org.prog3.project.Protocols;
 import org.prog3.project.Message.Message;
 
 public interface Protocol {
+    enum Type {
+        PING,
+        PONG,
+        NORMAL
+    }
     public void digest (Message message);
 }
