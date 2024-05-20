8199f6a3f37752ccbcccf7283f44bb814c48e699
Joana311 <deptop725+3@gmail.com>
2024-05-21 00:13:56 +0200

    changed constructor (maybe was not committed but was a mistake, need to implement digest()

--- src/main/java/org/prog3/project/Protocols/BlockchainProtocol.java
+++ src/main/java/org/prog3/project/Protocols/BlockchainProtocol.java
@@ -10,25 +10,25 @@ import org.prog3.project.Network.Peer;
 @Setter
 public class BlockchainProtocol implements Protocol {
     private NetworkManager networkManager;
+    private Type type;
 
     public enum Type {
         PING, PONG, REQUEST, RESPONSE
     }
 
-    private Type type;
-
-    public BlockchainProtocol(Type type, NetworkManager networkManager) {
+    public BlockchainProtocol(NetworkManager networkManager) {
         this.networkManager = networkManager;
-        this.type = type;
     }
 
     @Override
     public void digest(Message message) {
-
+        // TODO: based on which protocol is used in message (the received one), if PING initialize peerDoscovery
+        // If request, create message and send it to the peer of the message
+        // how idk yet, but step closer
     }
 
     @Override
     public void reply(Message message, Peer peer) {
-
+        peer.send(message);
     }
 }
